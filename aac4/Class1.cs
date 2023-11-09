13679c03f54a3b5a6543b8e4605dafad28ee71c1
4z0t <gastec139@gmail.com>
2023-11-09 21:24:18 +0300

    cleanup

--- aac4/Class1.cs
+++ aac4/Class1.cs
@@ -1,12 +1,7 @@
 ﻿using Microsoft.Win32;
-using System;
-using System.Collections.Generic;
 using System.Data;
-using System.IO;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
-using System.Windows;
 using System.Collections;
 
 namespace TA4
